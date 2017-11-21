4f35f18885d0fad71260959a545173b7da0ac0db
Lucie Keunen <lucie@keunen.net>
2017-11-21 13:40:51 +0100

    Add gem faker and created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+  Task.create(title: Faker::Job.title, description: Faker::ChuckNorris.fact)
+end
